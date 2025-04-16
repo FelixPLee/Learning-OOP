094dd7c8a358a29d88e4fcd7e8705d15d4e4c4e7
FelixPLee <felixpinheirolee@gmail.com>
2025-04-16 08:01:57 -0300

    Prototypes

--- script.js
+++ script.js
@@ -7,14 +7,35 @@ const Person = function(firstName, birthYear) {
     
     //Never do this
     // this.calcAge = function() {
-    //     console.log(2037 - this.birthYear)
+    //     console.log(2025 - this.birthYear)
     // }
 }
 
 const felix = new Person('Felix', 2005)
 const lucio = new Person('Lucio', 2003)
-console.log(felix)
-console.log(lucio)
+console.log(felix, lucio)
 
 console.log(felix instanceof Person)
-console.log(123 instanceof Persons)
+console.log(123 instanceof Person)
+
+//Prototypes 
+Person.prototype.calcAge = function() {
+    console.log(2025 - this.birthYear)
+}
+
+felix.calcAge()
+
+// {} linked to prototype
+console.log(felix.__proto__)
+console.log(lucio.__proto__ === Person.prototype) //Returns true
+
+console.log(Person.prototype.isPrototypeOf(Person)) //Returns false????
+console.log(Person.prototype.isPrototypeOf(felix)) //Returns true
+
+//.prototypeOfLinkedObject
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(felix.species, lucio.species)
+
+console.log(felix.hasOwnProperty('firstName'))
+console.log(felix.hasOwnProperty('species'))
