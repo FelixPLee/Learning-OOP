d2fb95653c37ae0c49632b73a733b1d0f0722b7c
FelixPLee <felixpinheirolee@gmail.com>
2025-04-16 07:35:33 -0300

    Constructor functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+    
+    //Never do this
+    // this.calcAge = function() {
+    //     console.log(2037 - this.birthYear)
+    // }
+}
+
+const felix = new Person('Felix', 2005)
+const lucio = new Person('Lucio', 2003)
+console.log(felix)
+console.log(lucio)
+
+console.log(felix instanceof Person)
+console.log(123 instanceof Persons)
