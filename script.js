a6b8bb84bc7c463cd4e53a1fd1e8be689dda3b07
FelixPLee <felixpinheirolee@gmail.com>
2025-05-07 12:37:58 -0300

    bank setup to data privacy

--- script.js
+++ script.js
@@ -209,6 +209,42 @@ const jay = Object.create(StudentProto)
 jay.init('Jay', 2010, 'PP')
 
 
+
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner
+        this.currency = currency
+        this.pin = pin
+        this.movements = []
+        this.locale = navigator.language
+
+        console.log(`Thanks for opening an account ${owner}`)
+    }
+    deposit(val) {
+        this.movements.push(val)
+    }
+
+    withdraw(val) {
+        this.deposit(-val)
+    }
+
+    approveLoan(val) {
+        return true
+    }
+
+    requestLoan(val) {
+        if (this.approveLoan(val)) {
+            this.deposit(val)
+            console.log(`Loan approved`)
+        }
+    }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111)
+console.log(acc1)
+
+acc1.movements.push(250)
+
 // Coding Challenge #2
 
 /* 
